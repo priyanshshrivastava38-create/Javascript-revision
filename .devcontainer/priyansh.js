42a5a8cbc86ee9923a4fa26ffe7cbe6d1faf8df1
priyanshshrivastava38-create <priyanshshrivastava38@gmail.com>
2025-11-26 22:33:25 +0000

    Change in file

--- .devcontainer/priyansh.js
+++ .devcontainer/priyansh.js
@@ -1,2 +1,30 @@
 console.log("Priyansh shrivastava")
 console.log("Welcome to the Dev Container!")
+console.log("Priyansh shrivastava")
+console.log("Welcome to the Dev Container!")
+console.log("Priyansh is good to know javascript")
+let age = "33abc"
+console.log(typeof age);
+let valueInnumber = Number(age)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+
+//1= true and 0=false in boolean type conversion
+// "" empty => when converted into boolean becomes false
+// "Priyansh" => when converted into boolean becomes true
+// null => false
+//let someNumber = 33
+/
+
+//1= true and 0=false in boolean type conversion
+// "" empty => when converted into boolean becomes false
+// "Priyansh" => when converted into boolean becomes true
+// null => false
+
