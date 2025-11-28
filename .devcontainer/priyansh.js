b11e6f042674f3ea62f86e5b719fa205f7a6ea5a
priyanshshrivastava38-create <priyanshshrivastava38@gmail.com>
2025-11-28 19:47:30 +0000

    Lastest changes

--- .devcontainer/priyansh.js
+++ .devcontainer/priyansh.js
@@ -21,10 +21,50 @@ console.log(typeof booleanIsLoggedIn);
 // "Priyansh" => when converted into boolean becomes true
 // null => false
 //let someNumber = 33
-/
+//
 
 //1= true and 0=false in boolean type conversion
 // "" empty => when converted into boolean becomes false
 // "Priyansh" => when converted into boolean becomes true
 // null => false
 
+//stack(primitive)-> number,string,boolean,null,undefined,bigint, symbol
+//heap(non-primitive)-> array, object, function
+
+let myyoutubename ="Priyanshshrivastava.com"
+
+
+let anothername = myyoutubename
+anothername ="Priyanshandcode"
+console.log(anothername)
+
+let user={
+    email:"user@google.com",
+    upi : "user@ybl",
+
+}
+let usertwo= user
+
+console.log(usertwo)
+console.log(user)
+
+let priyansh = "chaiisgood"
+console.log(typeof priyansh)
+
+const name ="Priyansh"
+let code2=["Priyansh","shrivastava","college","ucc","VITBHOPAL"]
+console.log(name)
+console.log(typeof code2)
+
+console.log(`hello my name is ${name} and my intro is ${code2}`);
+const gameName = new String("Priyansh")
+console.log(gameName[1])
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("i"))
+const newString = gameName.substring(0, 4)
+console.log(newString);
+const newstringone="    Hitesh   "
+console.log(newstringone)
+console.log(newstringone.trim());
+console.log(newstringone.includes("Hitesh"))
